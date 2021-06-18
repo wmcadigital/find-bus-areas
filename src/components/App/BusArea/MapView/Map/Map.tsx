2aee274c5e584dbb3109c6b699cd1039229af9a4
houbly <h.blyden@gmail.com>
2021-06-18 09:08:00 +0100

    Add base map

--- src/components/App/BusArea/MapView/Map/Map.tsx
+++ src/components/App/BusArea/MapView/Map/Map.tsx
@@ -1,9 +1,22 @@
+// Using https://developers.arcgis.com/javascript/latest/api-reference/ and ESRI JS API
+import { useRef } from 'react';
 import s from './Map.module.scss';
+// Import custom hooks for map functionality
+import useCreateMapView from './customHooks/useCreateMapView';
 
 const Map = () => {
+  // MAP SETUP
+  const mapContainerRef = useRef<any>();
+
+  useCreateMapView(mapContainerRef);
   return (
     <div className={`${s.mapView}`}>
-      <div className={s.mapContainer}>Map</div>
+      <div
+        id="disruptions-map"
+        className={`${s.mapContainer} webmap disruptions-esri-map`}
+        ref={mapContainerRef}
+        title="Disruptions map"
+      />
     </div>
   );
 };
